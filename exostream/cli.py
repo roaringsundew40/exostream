50afa8a58eb60d7b83a68ab4559db58f7c9cfbbc
Naman Khurana <namankhurana16@gmail.com>
2025-11-26 14:36:13 -0800

    bug fixes

--- exostream/cli.py
+++ exostream/cli.py
@@ -1,9 +1,12 @@
 """Main CLI entry point for Exostream"""
 
 # Import the new CLI implementation
-from exostream.cli.main import cli as main
+from exostream.cli.main import cli
+
+# Export cli as main for setuptools entry point
+main = cli
 
 
 if __name__ == '__main__':
-    main(obj={})
+    cli(obj={})
 
