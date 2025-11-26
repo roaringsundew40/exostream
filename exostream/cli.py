bec36e3b9addb63356e6d3d3785d4a4242aff5b2
Naman Khurana <namankhurana16@gmail.com>
2025-11-26 14:29:52 -0800

    test service based architecture

--- exostream/cli.py
+++ exostream/cli.py
@@ -1,20 +1,9 @@
 """Main CLI entry point for Exostream"""
 
-import click
-from exostream.sender.cli import send
-
-
-@click.group()
-@click.version_option(version="0.2.0")
-def main():
-    """Exostream - Stream webcam from Raspberry Pi using NDI"""
-    pass
-
-
-# Add subcommands
-main.add_command(send)
+# Import the new CLI implementation
+from exostream.cli.main import cli as main
 
 
 if __name__ == '__main__':
-    main()
+    main(obj={})
 
